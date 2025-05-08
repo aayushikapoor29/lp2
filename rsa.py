34db82d785916230989bb28ce72cd8649ad12e3b
Aayushi Kapoor <kapooraayushi2903@gmail.com>
2025-05-08 07:29:35 +0530

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,24 +1,25 @@
 import math
 
-def generate_keypair(p. q):
+def generate_keypair(p, q):
     n = p*q
     phi = (p-1)*(q-1)
     e = 2
 
     while math.gcd(e, phi) != 1: e+=1
-        d = pow(e, -1, phi)
-return (e,n), (d, n)
+    d = pow(e, -1, phi)
+    return (e,n), (d, n)
 
 def encrypt(msg, e, n):
-    return [pow(ord(chr), e, n) for char in msg]
+    return [pow(ord(char), e, n) for char in msg]
 
 def decrpyt(msg, d, n):
-    return ''.join([chr(pow(chr, d, n)) for chr in msg]
+    return ''.join([chr(pow(char, d, n)) for char in msg])
 
-p = 7
-q = 3
+p = 61
+q = 53
 
 public_key, private_key = generate_keypair(p, q)
-encrpyted = encrpyt('aayushikapoor', public_key[0], private_key[1])
+encrypted = encrypt('aayushikapoor', public_key[0], private_key[1])
 decrpyted = decrpyt(encrypted, private_key[0], public_key[1])
 
+print(encrypted, decrpyted)
